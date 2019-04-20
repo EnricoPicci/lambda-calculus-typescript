2ee6afdbb6a436d2df22c422bcbe7876d208da15
penrico <enrico.piccinin@gmail.com>
2019-04-20 23:04:10 +0200

    problem solution expanded

--- examples/problem-solution.ts
+++ examples/problem-solution.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { NUMBER } from '../src/natural-numbers/number';
 import { ZERO } from '../src/natural-numbers/_0_';
 import { ONE } from '../src/natural-numbers/_1_';
 import { SUCC } from '../src/mathematical-operators/successor';
@@ -20,7 +21,7 @@ describe('Build the solution to the problem', () => {
     // So, if m equals n, than
     // (EQ(m)(n)) (ADD(m)(n)) (SUB(m)(n))
     // returns The opposite happens if m is not equal to n
-    const PROBLEM_SOLUTION = m => n => EQ(m)(n)(ADD(m)(n))(SUB(m)(n));
+    const PROBLEM_SOLUTION = (m: NUMBER) => (n: NUMBER) => EQ(m)(n)(ADD(m)(n))(SUB(m)(n)) as NUMBER;
     it('m and n are ONE, i.e. they are equal and therefore they get summed', () => {
         const f: λ = x => x + 1;
         const a = 0;
@@ -49,4 +50,62 @@ describe('Build the solution to the problem', () => {
         const applyZEROToArgs = ZERO(f)(a);
         expect(applyProbSolToArgs).to.equal(applyZEROToArgs);
     });
+});
+
+describe('Expand the solution to show only anonymous lambdas', () => {
+    // We have built the solution of this problem without ever using and Typescript boolean or arithmetic logic
+    // rather just using lambdas
+    // type λ = (x: any) => any;
+    //
+    // if we remove the convenient variables we have used to identify the various functions used, e.g.
+    // EQ, ADD, SUB, PRED, AND, TRUE, FALSE etc. we get to
+    const SOL = m => n =>
+        (m => n =>
+            (p => q => p(q)(p))(
+                (m => n =>
+                    (n => n(x => a => b => b)(a => b => a))(
+                        (m => n => n(n => f => x => n(g => h => h(g(f)))(u => x)(u => u))(m))(m)(n),
+                    ))(m)(n),
+            )(
+                (m => n =>
+                    (n => n(x => a => b => b)(a => b => a))(
+                        (m => n => n(n => f => x => n(g => h => h(g(f)))(u => x)(u => u))(m))(m)(n),
+                    ))(n)(m),
+            ))(m)(n)((m => n => f => x => n(f)(m(f)(x)))(m)(n))(
+            (m => n => n(n => f => x => n(g => h => h(g(f)))(u => x)(u => u))(m))(m)(n),
+        );
+
+    it('m and n are ONE, i.e. they are equal and therefore they get summed', () => {
+        const f: λ = x => x + 1;
+        const a = 0;
+        const probSol = SOL(ONE)(ONE);
+        const applyProbSolToArgs = probSol(f)(a);
+        const TWO = SUCC(ONE);
+        const applyTWOToArgs = TWO(f)(a);
+        expect(applyProbSolToArgs).to.equal(applyTWOToArgs);
+    });
+    it('m is TWO and n is ONE, i.e. they are not equal and therefore we subtract n from m and the result is ONE', () => {
+        const f: λ = x => x + 1;
+        const a = 0;
+        const TWO = SUCC(ONE);
+        const probSol = SOL(TWO)(ONE);
+        const applyProbSolToArgs = probSol(f)(a);
+        const applyONEToArgs = ONE(f)(a);
+        expect(applyProbSolToArgs).to.equal(applyONEToArgs);
+    });
+    it(`m is ONE and n is TWO, i.e. they are not equal and therefore we subtract n from m and the result is ZERO
+        since SUB returns ZERO if n is greater than m`, () => {
+        const f: λ = x => x + 1;
+        const a = 0;
+        const TWO = SUCC(ONE);
+        const probSol = SOL(ONE)(TWO);
+        const applyProbSolToArgs = probSol(f)(a);
+        const applyZEROToArgs = ZERO(f)(a);
+        expect(applyProbSolToArgs).to.equal(applyZEROToArgs);
+    });
+
+    // Loking at this final result, one may question whether functional programming actually leads to more
+    // readable code, and probably there is a part of truth here. If brought to the extremes, this style of code
+    // can become easly obscure.
+    // At the same time can be pretty powerful.
 });
